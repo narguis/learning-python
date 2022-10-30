f554f0e4a0425c0ac1cde33817199a510748a93b
Guilherme Narguis <107137104+narguis@users.noreply.github.com>
2022-10-30 17:51:12 -0300

    Add files via upload

---
+++ ex099.py
@@ -0,0 +1,10 @@
+def maior(* núm):
+    print('-=' * 30)
+    print(f'Os valores {núm} foram informados, totalizando {len(núm)} valores ao todo.')
+    print(f'O maior valor informado foi {max(núm)}.')
+
+
+
+maior(2, 7, 8, 10)
+maior(1, 9, 901, 29, 348, 38932, 2012, 3, 4)
+maior(9 , 13 , 2, 3, 6, 7, 2, 2, 2, 1, 4)
